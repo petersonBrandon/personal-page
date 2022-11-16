651642537d133205c9a3bed3a767360f8d587bfa
Brandon Peterson <brandonacfuntech@gmail.com>
2022-11-16 13:30:52 -0600

    Started projects page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
+import { motion } from "framer-motion";
 import Skill from "../resources/Skill";
 import LoopingCarousel from "../resources/LoopingCarousel";
 import Navigation from "../resources/Navigations";
@@ -23,9 +24,18 @@ import github_logo from "../public/skills/github_logo.png";
 import linux_logo from "../public/skills/linux_logo.png";
 import jira_logo from "../public/skills/jira_logo.png";
 import git_logo from "../public/skills/git_logo.png";
+import cucumber_logo from "../public/skills/cucumber_logo.png";
+import selenium_logo from "../public/skills/selenium_logo.png";
+import appium_logo from "../public/skills/appium_logo.png";
+
+import codex from "../public/projects/codex.png";
+import zen_garden from "../public/projects/zen_garden.png";
+import time_sheet from "../public/projects/time_sheet.png";
 
 export default function Home() {
-  const [hydrated, setHydrated] = React.useState(false);
+  const [hydrated, setHydrated] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const ROWS = 3;
   const DURATION = 40000;
@@ -47,6 +57,9 @@ export default function Home() {
     { image: git_logo, title: "Git" },
     { image: linux_logo, title: "Linux" },
     { image: jira_logo, title: "Jira" },
+    { image: cucumber_logo, title: "Cucumber" },
+    { image: selenium_logo, title: "Selenium" },
+    { image: appium_logo, title: "Appium" },
   ];
 
   const shuffle = (arr: any) => [...arr].sort(() => 0.5 - Math.random());
@@ -77,55 +90,147 @@ export default function Home() {
           </div>
         </section>
         <section className="view" id="about_me_view">
-          <h1 className="view_title" id="about_me_title">
-            ABOUT ME
-          </h1>
-          <div id="about_content">
-            <div id="about_image_wrapper">
-              <Image
-                src={profilePic}
-                alt="Brandon Peterson"
-                width={3000}
-                height={3000}
-              />
+          <div className="view_content">
+            <h1 className="view_title" id="about_me_title">
+              ABOUT ME
+            </h1>
+            <div id="about_content">
+              <div id="about_image_wrapper">
+                <Image
+                  src={profilePic}
+                  alt="Brandon Peterson"
+                  width={3000}
+                  height={3000}
+                />
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
           </div>
         </section>
         <section className="view" id="skills_view">
-          <h1 className="view_title">SKILLS</h1>
-          <div id="skills_content">
-            {[...new Array(ROWS)].map((_, i) => (
-              <LoopingCarousel
-                key={i}
-                duration={random(DURATION - 10000, DURATION + 10000)}
-              >
-                {shuffle(SKILLS).map((skill) => {
-                  return (
-                    <Skill
-                      key={skill.title}
-                      image={skill.image}
-                      title={skill.title}
-                    />
-                  );
-                })}
-              </LoopingCarousel>
-            ))}
-            <div className="fade" />
+          <div className="view_content">
+            <h1 className="view_title">SKILLS</h1>
+            <div id="skills_content">
+              {[...new Array(ROWS)].map((_, i) => (
+                <LoopingCarousel
+                  key={i}
+                  duration={random(DURATION - 10000, DURATION + 10000)}
+                >
+                  {shuffle(SKILLS).map((skill) => {
+                    return (
+                      <Skill
+                        key={skill.title}
+                        image={skill.image}
+                        title={skill.title}
+                      />
+                    );
+                  })}
+                </LoopingCarousel>
+              ))}
+              <div className="fade" />
+            </div>
           </div>
         </section>
         <section className="view" id="projects_view">
-          <div id="home_header">
-            <h1 id="home_title">BRANDON PETERSON</h1>
-            <h3 id="home_subtitle">SOFTWARE ENGINEER</h3>
+          <div className="view_content">
+            <h1 className="view_title">PROJECTS</h1>
+            <div id="projects_background">
+              <div id="projects_content">
+                <motion.div
+                  layout
+                  data-isOpen={isOpen}
+                  whileHover={{
+                    scale: 1.05,
+                  }}
+                  className="project_tile"
+                  onClick={() => {
+                    setIsOpen(!isOpen);
+                  }}
+                >
+                  <div className="project_image_wrapper">
+                    <Image
+                      src={zen_garden}
+                      alt="CSS Zen Garden"
+                      height={2000}
+                      width={2000}
+                    />
+                  </div>
+                  <div className="project_body">
+                    <h1>CSS Zen Garden</h1>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </div>
+                </motion.div>
+                <motion.div
+                  whileHover={{
+                    scale: 1.05,
+                  }}
+                  className="project_tile"
+                  id="primary_project_tile"
+                >
+                  <div className="project_image_wrapper">
+                    <Image src={codex} alt="Codex" height={2000} width={2000} />
+                  </div>
+                  <div className="project_body">
+                    <h1>Codex</h1>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </div>
+                </motion.div>
+                <motion.div
+                  whileHover={{
+                    scale: 1.05,
+                  }}
+                  className="project_tile"
+                >
+                  <div className="project_image_wrapper">
+                    <Image
+                      src={time_sheet}
+                      alt="Time Sheet"
+                      height={2000}
+                      width={2000}
+                    />
+                  </div>
+                  <div className="project_body">
+                    <h1>Time Sheet</h1>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </div>
+                </motion.div>
+              </div>
+            </div>
           </div>
         </section>
         {/* <section className="view" id="certifications_view">
--- resources/Skill.tsx
+++ resources/Skill.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import Image, { StaticImageData } from "next/image";
+import { motion } from "framer-motion";
 
 type props = {
   image: StaticImageData;
@@ -8,12 +9,18 @@ type props = {
 
 const Skill: FC<props> = ({ image, title }) => {
   return (
-    <div className="skill_wrapper">
+    <motion.div
+      whileHover={{
+        rotate: [0, -5, 10, -10, 5, 0],
+        transition: { duration: 0.5 },
+      }}
+      className="skill_wrapper"
+    >
       <div className="skill_image_wrapper">
         <Image src={image} alt={title} width={2000} height={2000} />
       </div>
       <h4>{title}</h4>
-    </div>
+    </motion.div>
   );
 };
 
