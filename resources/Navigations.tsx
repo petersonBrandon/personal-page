5b5d22661eb7b648f5d218f9596d6b266f61ada6
Brandon Peterson <brandonacfuntech@gmail.com>
2022-11-29 23:06:34 -0600

    Fixed hamburger on final view

--- resources/Navigations.tsx
+++ resources/Navigations.tsx
@@ -3,6 +3,7 @@ import React, { FC, useEffect, useState } from "react";
 const Navigation: FC = () => {
   const [navDisplay, setNavDisplay] = useState(false);
   const [hamburgerDisplay, sethamburgerDisplay] = useState(false);
+  const [hamburgerColor, setHamburgerColor] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
@@ -11,6 +12,12 @@ const Navigation: FC = () => {
         sethamburgerDisplay(true);
       } else {
         sethamburgerDisplay(false);
+      }
+
+      if (window.scrollY >= window.innerHeight * 4 - 10) {
+        setHamburgerColor(true);
+      } else {
+        setHamburgerColor(false);
       }
     };
 
@@ -28,9 +35,24 @@ const Navigation: FC = () => {
         onClick={() => setNavDisplay(!navDisplay)}
       >
         <div id="hamburger_icon" className={navDisplay ? "hamburger_open" : ""}>
-          <div className="hamburger_line" id="hamburger_top" />
-          <div className="hamburger_line" id="hamburger_middle" />
-          <div className="hamburger_line" id="hamburger_bottom" />
+          <div
+            className={`hamburger_line ${
+              hamburgerColor ? "ham_light" : "ham_dark"
+            }`}
+            id="hamburger_top"
+          />
+          <div
+            className={`hamburger_line ${
+              hamburgerColor ? "ham_light" : "ham_dark"
+            }`}
+            id="hamburger_middle"
+          />
+          <div
+            className={`hamburger_line ${
+              hamburgerColor ? "ham_light" : "ham_dark"
+            }`}
+            id="hamburger_bottom"
+          />
         </div>
       </div>
       <div
